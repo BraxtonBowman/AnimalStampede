91cf3de4c660237e8de68cf9348c640de745812a
Braxton Bowman <10923641@my.uvu.edu>
2022-04-05 04:34:08 -0600

    Completed "Lesson 2.4 - Collision Decisions"

--- AnimalStampede/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
+++ AnimalStampede/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
@@ -10,17 +10,22 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound) {
-            Destroy(gameObject);
-        } else if (transform.position.z < lowerBound) {
+        //If object passes player's view, remove object
+        if (transform.position.z > topBound)
+        {
             Destroy(gameObject);
         }
-        
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+
     }
 }
